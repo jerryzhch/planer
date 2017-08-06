772486c8bd588a156a149d4046e651256962368b
jerryzhch <jerry_agboola@hotmail.com>
2017-08-06 20:09:35 +0200

    Focussing on grades now

--- www/js/login.js
+++ www/js/login.js
@@ -17,41 +17,5 @@
 	const btnSignUp= document.getElementById('btnSignUp');
 	const btnLogout = document.getElementById('btnLogout');
 
-	// Add login event
-	btnLogin.addEventListener('click', e => {
-		//Get Email and pass
-		const email = txtEmail.value;
-		const pass = txtPassword.value;
-		const auth = firebase.auth();
-		// Sign In
-		const promise = auth.signInWithEmailAndPassword(email, pass);
-		promise.catch(e => console.log(e.message));
-	});
 
-	// Add signup event
-	btnLogin.addEventListener('click', e => {
-		//Get Email and pass
-		// TODO: CHECK 4 REAL EMAIL
-		const email = txtEmail.value;
-		const pass = txtPassword.value;
-		const auth = firebase.auth();
-		// Sign In
-		const promise = auth.createUserWithEmailAndPassword(email, pass);
-		promise
-			.catch(e => console.log(e.message));
-	});
-
-  btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-  });
-
-
-	// Add a realtime Auth Listener
-	firebase.auth().onAuthStateChanged(firebaseUser => {
-		if(firebaseUser){
-			console.log(firebaseUser);
-		}else{
-			console.log('not logged in');
-		}
-	});
  }());
