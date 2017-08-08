892e0bd4089b43e1af255daf8125779f6da93704
jerryzhch <jerry_agboola@hotmail.com>
2017-08-08 19:54:32 +0200

    new app layout and some fixes

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,45 +1,74 @@
 // Initialize your app
 var myApp = new Framework7({
+  // Default title for modals
+  modalTitle: 'Planer',
 
-	material: true,
-	// enables hash navigation
-	pushState: true,
-	cache: true,
-	// Default title for modals
-	modalTitle: 'Planer',
-	router: true
+  // If it is webapp, we can enable hash navigation:
+  pushState: true,
+
+  // Hide and show indicator during ajax requests
+onAjaxStart: function (xhr) {
+    myApp.showIndicator();
+},
+onAjaxComplete: function (xhr) {
+    myApp.hideIndicator();
+}
 });
 
 // Export selectors engine
 var $$ = Dom7;
 
-
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-		domCache: true,
+    domCache: true
 });
 
-
-myApp.onPageInit('page-name-here',function(page){
-   $$(".mylink").on("click",function(e){
-      var myUrl = $$(this).data('myurl');
-      var popupHTML = "";
-
-      $$.ajax({
-         type:'GET',
-         url:myUrl,
-         dataType:'html',
-         success:function(data){
-               popupHTML = '<div class="popup">'+
-                    '<div class="content-block">'+
-                      '<p>' + data + '.</p>'+
-                      '<p><a href="#" class="close-popup">Close me</a></p>'+
-                    '</div>'+
-                  '</div>'
-
-               myApp.popup(popupHTML);
-            }
-         });
-   });
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
 });
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
+
+/*var userInput = document.getElementById("userInput");
+userInput.addEventListener("click", newsem);
+
+function newsem(){
+	var semName =
+	"<span class='ersetzung'>" +
+	document.getElementById("semName").value + "</span>";
+
+/*	var semesterlist;
+	semesterlist +=*/
+//}
