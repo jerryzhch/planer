ad20011f37cee548398022ccf75079a4be89f042
jerryzhch <jerry_agboola@hotmail.com>
2017-09-18 22:04:50 +0200

    finally implementing database services to app

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,59 @@
  (function() {
  // Initialize Firebase
-	 var config = {
-		apiKey: "AIzaSyD5MjBa0qzkpwzN0ThYAuvk8_pqXDG8Ibc",
-		authDomain: "matur18-90b73.firebaseapp.com",
-		databaseURL: "https://matur18-90b73.firebaseio.com",
-		projectId: "matur18-90b73",
-		storageBucket: "",
-		messagingSenderId: "268822264260"
-		};
-	 firebase.initializeApp(config);
+ var config = {
+   apiKey: "AIzaSyD5MjBa0qzkpwzN0ThYAuvk8_pqXDG8Ibc",
+   authDomain: "matur18-90b73.firebaseapp.com",
+   databaseURL: "https://matur18-90b73.firebaseio.com",
+   projectId: "matur18-90b73",
+   storageBucket: "matur18-90b73.appspot.com",
+   messagingSenderId: "268822264260"
+ };
+ firebase.initializeApp(config);
+   // Get elements
+   var preObject = document.getElementById('object');
+   var ulList = document.getElementById('list');
 
+   // Create references
+   var dbRefObject = firebase.database().ref().child('object');
+   var dbRefList = dbRefObject.child('hobbies');
+
+   // Sync object changes
+   dbRefObject.on('value', snap =>{
+      preObject.innerText = JSON.stringify(snap.val(), null, 3);
+   });
+
+   // Sync List changes
+   dbRefList.on('child_added', snap => {
+
+     var li = document.createElement("li");
+     li.innnerText = snap.val();
+     li.id = snap.key;
+     ulList.appendChild(li);
+
+   });
+
+   dbRefList.on('child_changed', snap => {
+
+     var liChanged = document.getElementById(snap.key);
+     liChanged.innnerText = snap.val();
+
+   });
+
+   dbRefList.on('child_removed', snap => {
+
+     var liToRemove = document.getElementById(snap.key);
+     liToRemove.remove();
+
+   });
+
+
+
+
+
+
+
+
+   // *** LOGIN ***
 	// Get elememts
 	const txtEmail = document.getElementById('txtEmail');
 	const txtPassword = document.getElementById('txtPassword');
